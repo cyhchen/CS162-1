d8288dbc6628241d1717f6863102db070e9535a6
linlianhui <824493035@qq.com>
2019-08-18 02:02:23 +0000

    Finish adding basic functionality into the shell.

--- hw1/shell.c
+++ hw1/shell.c
@@ -29,6 +29,8 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
+int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -43,6 +45,8 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
+  {cmd_pwd, "pwd", "print the current working directory"},
+  {cmd_cd, "cd", "change the current working directory to the directory"},
 };
 
 /* Prints a helpful description for the given command */
@@ -55,6 +59,28 @@ int cmd_help(unused struct tokens *tokens) {
 /* Exits this shell */
 int cmd_exit(unused struct tokens *tokens) {
   exit(0);
+}
+
+int cmd_pwd(unused struct tokens *tokens) {	
+	char buf[1024];
+	getcwd(buf, 1024);
+	printf("%s\n", buf);
+	return 1;
+}
+
+int cmd_cd(unused struct tokens *tokens) {	
+	char* token = tokens_get_token(tokens, 1);
+	if(token == NULL) {	
+		printf("please input argument\n");
+		return 0;
+	}
+	
+	if(chdir(token) == 0)
+		return 1;
+	else{	
+		printf("cd: no such file or directory: %s\n", token);
+		return 0;
+	}
 }
 
 /* Looks up the built-in command, if it exists. */
